c78c087d1727313dc4ce5ac0f061229674d8a39b
rafael-brickeye <113912295+rafael-brickeye@users.noreply.github.com>
2023-06-08 13:56:53 -0400

    Add files via upload

---
+++ components/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+function TestComponent() {
+  return (
+    <View>
+      <Text>Hello World</Text>
+    </View>
+  );
+}
+
+export {TestComponent};
